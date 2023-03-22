a483e742aadc70a143ebb10ba964c03c5f6709eb
Bong DeGuzman <79473084+BDevelopment1@users.noreply.github.com>
2023-03-22 18:18:39 -0500

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,35 @@
+CreateThread(function()
+    while true do
+        local sleep = 500
+
+        local ped = PlayerPedId()
+        local coords = GetEntityCoords(ped)
+
+        for k, v in pairs(Config.Parmacystore) do
+
+            local OFFLINEDist = #(coords - v.position)
+
+            if OFFLINEDist <= 1.0 then
+                sleep = 0
+                lib.showTextUI('[E] '.. v.name)
+                if IsControlJustReleased(0, 38) then
+                    ESX.Progressbar(500, 'Checking EMS Duty...', 'info', false)
+                    sleep = 500
+                    ESX.TriggerServerCallback('BONGCOUNT-JOB', function(EMSCount)
+                        if EMSCount < 1 then
+                          ox_inventory:openInventory('shop', {type = 'Pharmacy'})
+                            -- NOTIFY SUCCESS
+                        else
+                            -- NOTIFY ERROR
+                        end
+                    end, 'ambulance')
+                end
+            elseif OFFLINEDist <= 5.0 then
+                sleep = 0
+                lib.hideTextUI()
+                ESX.DrawMarker(v.position, 255, 255, 255, 255)
+            end 
+        end
+        Wait(sleep)
+    end
+end)
